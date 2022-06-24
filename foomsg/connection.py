86ba09c9637aee331bbc0944807d479cbee261f7
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-06-24 14:55:53 -0600

    Use iscoroutinefunction instead of iscoroutine

--- foomsg/connection.py
+++ foomsg/connection.py
@@ -162,7 +162,7 @@ class _Connection:
     async def notify_message_listeners(self, message: MessageOrRequest):
         event_loop = asyncio.get_event_loop()
         for callback in self._message_listeners:
-            if asyncio.iscoroutine(callback):
+            if asyncio.iscoroutinefunction(callback):
                 event_loop.create_task(callback(message))
             else:
                 callback(message)
